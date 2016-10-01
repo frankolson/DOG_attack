de7ea333392302fc6929b58484d2ef5156c36aa4
Frank Olson <will.olson11@gmail.com>
2016-09-30 19:28:21 -0700

    added settings file

---
+++ settings.py
@@ -0,0 +1,8 @@
+#
+# File to store or the variables that may to need be changed frequently
+#
+from secret import *
+
+## System Settings
+SLACK_CHANNEL  = "#hoodhunting"
+SLEEP_INTERVAL = 20 * 60 # 20 minutes
