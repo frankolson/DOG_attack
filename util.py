df380a57ee123f179a7d9963f7a7e68156322c9c
Frank Olson <will.olson11@gmail.com>
2016-10-09 18:11:31 -0700

    added twilio function

--- util.py
+++ util.py
@@ -6,19 +6,29 @@ import urllib2
 import json
 from time import sleep
 from slackclient import SlackClient
+from twilio.rest import TwilioRestClient
 
 # post to slack
 def post_to_slack(slack_client, message):
-
     slack_client.api_call(
         "chat.postMessage", channel=settings.SLACK_USER, text=message,
         username='DOGgie', icon_emoji=':dog:'
     )
 
+# send Twilio text
+def text_with_twilio(twilio_client, message):
+    message = client.messages.create(
+        to=settings.TO_PHONE,
+        from_=settings.FROM_PHONE,
+        body=message
+    )
+
 # run attack
 def dog_attack():
-    # create slack client
+    # create slack and twilio clients
     slack_client = SlackClient(settings.SLACK_TOKEN)
+    twilio_client = TwilioRestClient(settings.TWILIO_ACCOUNT_SID,
+                                     settings.TWILIO_AUTH_TOKEN)
 
     # send 100 messages
     for _ in range(100):
@@ -29,6 +39,8 @@ def dog_attack():
 
         # post to slack
         post_to_slack(slack_client, message)
+        # text with twilio
+        text_with_twilio(twilio_client, message)
 
         # adhere to slack rate limiting
         sleep(1.1)
